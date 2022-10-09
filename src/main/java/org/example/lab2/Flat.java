0e1d5b80e446a25043d94f116f211c3379e59a2a
Pakshaev <pakshaev2003@gmail.com>
2022-10-09 16:08:47 +0300

    2

---
+++ src/main/java/org/example/lab2/Flat.java
@@ -0,0 +1,43 @@
+package org.example.lab2;
+
+public class Flat {
+
+    static int flatSquare;
+    static int roomsAmount;
+
+    public Flat()
+    {
+        final int baseRAmount = 2;
+        final int baseFSquare = 50;
+    }
+
+    public Flat()
+    {
+
+    }
+
+    public Flat()
+    {
+
+    }
+
+    public void getRoomsAmount()
+    {
+        return;
+    }
+
+    public void changeRoomsAmount()
+    {
+
+    }
+
+    public void getFlatSquare()
+    {
+        return;
+    }
+
+    public void changeFlatSquare()
+    {
+
+    }
+}
